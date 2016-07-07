3db9040cf8e96e44412c5d1d87ad7e59b67c4436
brcolligan <brcolliganmisc@gmail.com>
2016-07-06 22:47:31 -0400

    updated settings.py

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -112,7 +112,7 @@ WSGI_APPLICATION = 'learning_log.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
-    DATABASES = {
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
