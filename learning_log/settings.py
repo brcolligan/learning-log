4c0a62c810cd0133c632f8db9c627ca89932585c
brcolligan <brcolliganmisc@gmail.com>
2016-07-06 22:43:27 -0400

    added ON_HEROKU to DATABASES in settings.py

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -112,7 +112,11 @@ WSGI_APPLICATION = 'learning_log.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
-DATABASES = {
+if ON_HEROKU == True:
+    import dj_database_url
+    DATABASES = {'default': dj_database_url.config()}
+else:
+    DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
